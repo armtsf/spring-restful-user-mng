b9ab5b597e4aecb70238f239ba445c8b45e2bb88
armita <armt.safa@gmail.com>
2018-07-01 14:15:52 +0430

    first commit

---
+++ src/test/java/ir/restusrmng/RestfulUserManagement/RestfulUserManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.restusrmng.RestfulUserManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulUserManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
