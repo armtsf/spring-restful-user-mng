5f843e2b117a7a78ae2daea6926e3f1f27b2c4ed
armita <armt.safa@gmail.com>
2018-07-04 14:20:28 +0430

    user service unit tests added

--- src/test/java/ir/restusrmng/RestfulUserManagement/UserServiceImplTest.java
+++ src/test/java/ir/restusrmng/RestfulUserManagement/UserServiceImplTest.java
@@ -10,57 +10,51 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.mock.http.client.MockClientHttpRequest;
-import org.springframework.mock.http.client.MockClientHttpResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class RestfulUserManagementApplicationTests {
+public class UserServiceImplTest {
 
-	@Mock
-	private UserRepository userRepository;
+    @MockBean
+    private UserRepository userRepository;
 
-    @InjectMocks
-    private UserServiceImpl userServiceImpl;
+    @Autowired
+    private UserService userServiceImpl;
 
-	@Before
-	public void setUp() {
-        MockitoAnnotations.initMocks(this);
-		User alex = new User("alex", "123");
-		Mockito.when(userRepository.findByUsername(alex.getUsername())).thenReturn(alex);
-	}
+    @TestConfiguration
+    static class UserServiceImplTestContextConfiguration {
 
-	@Test
-	public void whenUserInRepository_thenUserShouldBeFound() {
-		String name = "alex";
-		User found = userServiceImpl.findByUsername(name);
-		assertEquals(found.getUsername(), name);
-	}
+        @Bean
+        public UserServiceImpl employeeService() {
+            return new UserServiceImpl();
+        }
+    }
 
-	@Test(expected = UserNotFoundException.class)
+    @Before
+    public void setUp() {
+       // MockitoAnnotations.initMocks(this);
+        User alex = new User("alex", "123");
+        Mockito.when(userRepository.findByUsername(alex.getUsername())).thenReturn(alex);
+    }
+
+    @Test
+    public void whenUserInRepository_thenUserShouldBeFound() {
+        String name = "alex";
+        User found = userServiceImpl.findByUsername(name);
+        assertEquals(found.getUsername(), name);
+    }
+
+    @Test(expected = UserNotFoundException.class)
     public void  whenUserNotInRepository_ifUserNotFound_thenShouldThrowUserNotFoundException() {
         String name = RandomStringUtils.randomAlphabetic( 8 );
-        User user = userServiceImpl.findByUsername(name);
+        userServiceImpl.findByUsername(name);
     }
 
     @Test
@@ -95,26 +89,26 @@ public class RestfulUserManagementApplicationTests {
 
     @Test
     public void whenUpdatingUser_ifUserExists_thenShouldUpdate() {
-	    User user = userServiceImpl.updateUser("alex", new User("ali", "123"));
-	    assertNotNull(user);
+        User user = userServiceImpl.updateUser("alex", new User("ali", "123"));
+        assertNotNull(user);
     }
 
     @Test(expected = UserNotFoundException.class)
     public void whenUpdatingUser_ifDoesNotExist_thenShouldThrowUserNotFoundException() {
         String name = RandomStringUtils.randomAlphabetic( 8 );
-	    User user = userServiceImpl.updateUser(name, new User("ali", "123"));
+        User user = userServiceImpl.updateUser(name, new User("ali", "123"));
     }
 
     @Test
     public void whenUserCredentialsAreCorrect_thenShouldLogin() {
-	    User user = userServiceImpl.login("alex", "123");
-	    assertNotNull(user);
+        User user = userServiceImpl.login("alex", "123");
+        assertNotNull(user);
     }
 
     @Test(expected = AuthenticationException.class)
     public void whenUsernameNotValid_thenShouldThrowAuthenticationException() {
         String name = RandomStringUtils.randomAlphabetic( 8 );
-	    User user = userServiceImpl.login(name, "123");
+        User user = userServiceImpl.login(name, "123");
     }
 
     @Test(expected = AuthenticationException.class)
